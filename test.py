b9bbde1ec66435fdd6544e2aa6f129637ac392e0
gaoguanlu <gaoguanlu@datagrand.com>
2022-11-21 14:40:33 +0800

    增加测试数据

--- test.py
+++ test.py
@@ -1,6 +1,22 @@
 from rmbconvert import Traditional
 
-if __name__ == "__main__":
-    rmb = Traditional("伍佰叁拾玖万零贰拾壹元叁角伍分")
-    number = rmb.to_number()
-    print(number)
+
+def test_traditional(values: list):
+    for value in values:
+        rmb = Traditional(value)
+        integer = rmb.to_number()
+        print("原始大写金额: [{}]  转换后的小写金额: [{}]".format(value, integer))
+
+
+upper_values = [
+    "伍佰叁拾玖万零贰拾壹元叁角伍分",
+    "柒仟陆佰零捌万玖仟贰佰叁拾壹元零贰分",
+    "叁拾贰万伍仟玖佰玖拾壹元整",
+    "壹仟零壹拾万壹仟零壹拾元陆角柒分",
+    "壹仟壹佰壹拾壹万壹仟壹佰壹拾壹元壹角壹分",
+]
+
+line = "*" * 20
+print(line, "大写转小写", line)
+test_traditional(upper_values)
+print("\n")
